7e3b911eff0c8fdc13f534be6a19a0fec5b7159b
Bhangure <bhangureasha853@gmail.com>
2023-01-11 08:29:03 +0530

    this is my app

---
+++ src/main/java/examples/Program3.java
@@ -0,0 +1,15 @@
+package examples;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Program3 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\selenium\\exe\\chromedriver_win32 (1)\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+        System.out.println("this is my test application");
+    }
+}
